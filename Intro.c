9fb5078879e4168fe2f9a2282471446880430844
Debanshu Maity <maitydebanshu33@gmail.com>
2026-07-31 23:10:14 +0530

    Add first C programs

---
+++ Intro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("My name is Debanshu Maity.\n");
+    printf("I study CSE at MIT Bangalore.\n");
+    printf("This is my first C-program.\n");
+    return 0;
+}
