8c653b21b20c443bc9a0e94ff773b689db43b57b
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-01 23:19:10 +0530

    Usage of char in Input command

--- input.c
+++ input.c
@@ -3,11 +3,15 @@ int main()
 {
     int age;
     float marks;
+    char grade;
     printf ("Enter your age: \n");
     scanf("%i", &age);
     printf("Enter your marks: \n");
     scanf("%f", &marks);
+    printf("Enter your Grade: \n");
+    scanf(" %c", &grade);
     printf("Your age is:%i \n", age);
-    printf(" Your marks are:%.2f", marks);
+    printf(" Your marks are:%.2f \n", marks);
+    printf("Your grade: %c", grade);
     return 0;
 }
