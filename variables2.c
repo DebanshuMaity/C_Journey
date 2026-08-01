bccc192cab3388aefdc196342da2367fb6cfa608
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-01 20:10:48 +0530

    Variables2

---
+++ variables2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int age = 19;
+    float marks = 99.8;
+    char subject = 'C';
+    printf("Age : %i\n, Marks : %.1f\n, subject : %c\n", age, marks, subject );
+    return 0;
+}
