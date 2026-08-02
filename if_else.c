f57c4994e09acb45a073834a2bf412fee87c8f68
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-02 16:03:35 +0530

    Learning Logical Operators with if else

--- if_else.c
+++ if_else.c
@@ -6,17 +6,29 @@ int main()
     printf("Enter the Marks: ");
     scanf("%d", &marks);
 
-    if (marks >= 90)
+    if (marks < 0 || marks > 100)
     {
-        printf("Grade A\n");
+        printf("Invalid Marks!\n");
     }
-    else if (marks >= 75)
+    else if ( marks >= 90)
     {
-        printf("Grade B\n");
+        printf("Grade A");
     }
-    else
+    else if (marks >= 80)
     {
-        printf("Grade C.\n");
+        printf("Grade B");
+    }
+    else if ( marks >= 70)
+    {
+        printf("Grade C");
+    }
+    else if ( marks >= 60)
+    {
+        printf("Grade D");
+    }
+    else if ( marks >= 0)
+    {
+        printf("Grade F");
     }
     return 0;
 }
