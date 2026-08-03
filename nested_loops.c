6b0de50c3eb0d672c5bde5209456c13a0c01afc8
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-03 23:08:49 +0530

    nested_loops in C

--- nested_loops.c
+++ nested_loops.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int i, j;
-    for (i = 1; i <= 3; i++)
+   int i,j;
+   for (i=1; i<=3; i++)
+   {
+    for (j=1; j<=5; j++)
     {
-        for (j = 1; j <= 3; j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
+        printf("*");
     }
-    return 0;
+    printf("\n");
+   }
 }
