d298aa6ec9a2bf230a0c4061ebb1f5b5d979f7e1
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-03 22:44:49 +0530

    Learning Loops in C

--- loops.c
+++ loops.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1;
+    int i = 10;
     do
     {
-        printf("%d\n",i);
-        i++;
-    } 
-    while ( i<= 5);
-    return 0;    
+        printf("Hello\n");
+    }
+    while (i<5);    
 }
