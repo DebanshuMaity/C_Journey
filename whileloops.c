a03b8344d30831d3fff5f1fa123ec95bb50a1cab
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-03 22:33:24 +0530

    While_loops2 in C

--- whileloops.c
+++ whileloops.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int i=1;
-    while (i <=5)
+    int i=10;
+    while (i>=1)
     {
         printf("%d\n", i);
-        i++;
+        i = i-1;
     }
     return 0;
 }
