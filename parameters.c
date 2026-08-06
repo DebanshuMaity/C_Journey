76085d393aecdc3dac8ae4ef15a95fb1ec2a4219
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-06 19:52:57 +0530

    parameters_add

--- parameters.c
+++ parameters.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void add(int x, int y)
+void multiply(int x, int y)
 {
-    printf("%d\n", x+y);
+    printf("%d\n", x*y);
 }
 int main()
 {
-    add(7 , 8);
+    multiply(7 , 8);
     return 0;
 }
