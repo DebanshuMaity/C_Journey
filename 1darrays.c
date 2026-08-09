9f72c7e4ed3f3b125e36e35868ed5ebb09685eef
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-09 17:10:08 +0530

    1darrays_reverse

--- 1darrays.c
+++ 1darrays.c
@@ -2,29 +2,16 @@
 int main()
 {
     int numbers[5];
-    int i,target;
+    int i;
     printf("Array Numbers = \n ");
     for (i=0; i<5;i++)
     {
         scanf("%d",&numbers[i]);
     }
-    printf("target = \n");
-    scanf("%d", &target);
-    int found = 0;
-    for (i=0; i<5; i++)
+    printf("Reversed Array = \n");
+    for (i=4;i>=0; i--)
     {
-        if (numbers[i] == target)
-        {
-            found =1;
-        }
+        printf("%d\n", numbers[i]);
     }
-    if (found == 1)
-    {
-        printf("found!");
-    }
-    else
-    {
-        printf("not found !!");
-    }
-    return 0;     
+    return 0;
 }
