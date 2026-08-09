866c064030e70ca59708b0dc6f96ce7935650356
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-09 17:01:17 +0530

    1Darrays_linear search

--- 1darrays.c
+++ 1darrays.c
@@ -2,26 +2,29 @@
 int main()
 {
     int numbers[5];
-    int i,even,odd;
+    int i,target;
     printf("Array Numbers = \n ");
     for (i=0; i<5;i++)
     {
         scanf("%d",&numbers[i]);
     }
-    even = 0;
-    odd = 0;
+    printf("target = \n");
+    scanf("%d", &target);
+    int found = 0;
     for (i=0; i<5; i++)
     {
-        if (numbers[i]%2==0)
+        if (numbers[i] == target)
         {
-            even = even +1;
-        }
-        else
-        {
-            odd = odd +1;
+            found =1;
         }
     }
-    printf("no. of even numbers = %d\n", even);
-    printf("no. of even numbers = %d", odd);
-    return 0;   
+    if (found == 1)
+    {
+        printf("found!");
+    }
+    else
+    {
+        printf("not found !!");
+    }
+    return 0;     
 }
