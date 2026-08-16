18807e0a3815a0345cb7c78c1c3812692a8fe805
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-16 18:37:17 +0530

    strings_bugs_fixed

--- strings.c
+++ strings.c
@@ -4,9 +4,8 @@ int main()
 {
     int a;
     char words[6];
-    scanf("%s", words);
+    scanf("%5s", words);
     printf("%s\n", words);
-    a=strlen(words);
-    printf("length of word = %d", a);
+    printf("length of word = %zu", strlen(words));
 
 }
