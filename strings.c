713b611b6164e4dc1001a54ff818b856313f7b2c
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-17 20:23:00 +0530

    strings_palindrome

--- strings.c
+++ strings.c
@@ -3,12 +3,24 @@
 int main()
 {
     int i;
+    int palindrome = 1;
     char words[51];
     scanf("%50s", words);
     printf("%s\n", words);
-    for (i= strlen(words)-1 ; i>=0;i--)
+    for (i= 0; i< (strlen(words)/2) ;i++)
     {
-        printf("%c", words[i]);       
+        if (words[i]!= words[strlen(words)-1-i])
+        {
+            palindrome = 0;
+        }       
+    }
+    if (palindrome == 1)
+    {
+        printf("It's a palindrome");
+    }
+    else
+    {
+        printf("It's not a palindrome");
     }
     return 0;
 
