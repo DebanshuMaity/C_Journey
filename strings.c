60cb8f2e53d469f200c9af31b0c41917024e8405
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-17 20:05:05 +0530

    strings_reverse

--- strings.c
+++ strings.c
@@ -3,19 +3,13 @@
 int main()
 {
     int i;
-    int number = 0;
-    char target;
     char words[51];
-    scanf("%50s", &words);
+    scanf("%50s", words);
     printf("%s\n", words);
-    for (i=0; words[i]!='\0';i++)
+    for (i= strlen(words)-1 ; i>=0;i--)
     {
-        if (words[i]== 'a' || words[i]== 'e' || words[i]== 'i' || words[i]== 'o' || words[i]== 'u' )
-        {
-            number++;
-        }
+        printf("%c", words[i]);       
     }
-    printf("no. of time vowels appeared = %d", number);
     return 0;
 
 }
