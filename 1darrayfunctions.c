e02bbeb879b9242304b52485ecd2d40d823f9268
Debanshu Maity <maitydebanshu33@gmail.com>
2026-08-29 15:02:03 +0530

    1Darrayfunctions

--- 1darrayfunctions.c
+++ 1darrayfunctions.c
@@ -4,12 +4,21 @@ void display(int arr[],int size)
     int i;
     for (i=0 ; i< size; i++ )
     {
-        printf("%d",arr[i]);
+        arr[i] = arr[i]*2;
+        printf("%d ",arr[i]);
     }
 }
 int main()
 {
-    int numbers[5]={1,2,3,4,5};
+    int i;
+    int size;
+    printf("size: ");
+    scanf("%d ",&size);
+    int numbers[i];
+    for (i=0;i<size;i++)
+    {
+        scanf("%d",&numbers[i]);
+    }
     display(numbers , 5);
     return 0;
 }
